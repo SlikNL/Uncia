e0eb34ab430f05fbd8a88ba948d1ea1bdb22b04d
Denis Sokolov <denis@sokolov.cc>
2013-01-12 23:23:53 +0100

    Preg: test

---
+++ tests/Slik/Uncia/PregTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Slik\Uncia;
+
+class PregTest extends \PHPUnit_Framework_TestCase
+{
+	public function dataNotFound()
+	{
+		return array(
+			array('/([a-z]+)/', ' 23 23 223'),
+		);
+	}
+
+	public function dataSimple()
+	{
+		return array(
+			array('john', '/([a-z]+)/', ' 23 23 john223'),
+			array(array('john', 'snow'), '/([a-z]+) ([a-z]+)/', ' 23 23 john snow223'),
+		);
+	}
+
+	/**
+	 * @dataProvider dataNotFound
+	 */
+	public function testNotFound($pattern, $subject)
+	{
+		$this->setExpectedException('\\Slik\\Uncia\\Exception\\NotFound');
+		preg($pattern, $subject);
+	}
+
+	/**
+	 * @dataProvider dataSimple
+	 */
+	public function testSimple($target, $pattern, $subject)
+	{
+		$this->assertSame($target, preg($pattern, $subject));
+	}
+}
